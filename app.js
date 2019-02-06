6e7037874058ade46c2e8b2513487cb7b1817fb3
sergeymaguire <sergeymaguire@gmail.com>
2019-02-05 20:24:41 -0800

    make it better

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 
 sendSMS();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => console.log(`Listening on ${port}`));
 
